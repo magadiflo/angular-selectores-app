99ecd9122621e1a4091ea4e296ded25ca7460dfb
Martín <magadiflo@gmail.com>
2022-09-15 22:46:46 -0500

    Estructura de directorios para esta aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'paises',
+    loadChildren: () => import('./paises/paises.module').then(m => m.PaisesModule),
+  },
+  { path: '**', redirectTo: 'paises', }
 ];
 
 @NgModule({
